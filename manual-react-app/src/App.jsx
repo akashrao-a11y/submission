c04b50c0ed460949ed9516ab65171c472fc98bea
Akashrao J <akashrao.j@cspl.com>
2025-11-12 13:28:32 +0530

    app.jsx modify

--- manual-react-app/src/App.jsx
+++ manual-react-app/src/App.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Welcome from "./components/Welcome";
-import Thoughts from "./components/Thoughts";
 import FIX_ERROR_01 from "./components/FIX_ERROR_01";
 import PeopleData from "./components/PeopleData";
 
@@ -14,8 +12,8 @@ const App = () => {
         padding: "0 1rem",
       }}
     >
-      <Welcome />
-      <Thoughts />
+      
+     
       <PeopleData/>
       <FIX_ERROR_01/>
     </div>
